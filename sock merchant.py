7ec102c26c099e71f29692480bb0e18b52956841
bvishal8510 <baghel.vishal124124@gmail.com>
2018-09-04 22:30:33 +0530

    solved sock merchant problem

--- sock merchant.py
+++ sock merchant.py
@@ -3,8 +3,11 @@ array = list(map(int, input().split()))
 d = {}
 count = 0
 for i in array:
-	d[i] += 1
-	if d[i] == 2:
-		count += 1
-		d[i] = 0
+	if i in d.keys():
+		d[i] += 1
+		if d[i] == 2:
+			count += 1
+			d[i] = 0
+	else:
+		d[i] = 1
 print(count)

c848a1344ac2ef687d381530d4ec62cdc6a1b435
bvishal8510 <baghel.vishal124124@gmail.com>
2018-09-08 21:21:09 +0530

    made regex for student number valid for every year

--- regex.py
+++ regex.py
@@ -1,14 +1,28 @@
 import re
 import datetime
+
 # pattern = re.compile("^[1-2][0-9]{6}[dD]?$")
 #^[1][5-8]\d{5}[Dd]{0,1}$
-year1 = datetime.date.today().year
-year2 = year1 - 3
-year1 = str(year1)
-print(year1)
-print(type(year1))
-print(year2)
-st1 = "^[1][5-8]\d{5}[Dd]{0,1}$"
+#^[1][5-8][0-8][0-6][0-2][0-9][0-9][Dd]{0,1}$
+year = datetime.date.today().year
+end = ''
+start = ''
+
+for i in range(year, year-4, -1):
+	end += str(i % 10)
+	i = int(i/10)
+	start += str(i % 10)
+
+st1 = "^["+start+"]["+end+"][0-4][0-4][0-1][0-9][0-9][-]?[mdlMDL]?$"    		# regex for student number workable for every year
+print(st1)
 pattern = re.compile(st1)
-string = "1610181"
-print(pattern.match(string))
+# print(pattern.match(string))
+while(1):
+	string = input("Enter year :")
+	if (pattern.match(string)):
+		print("match")
+	else:
+		print("No match")
+		
+
+

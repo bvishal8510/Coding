af23953e91100009a9e728484ce7bf34f1703cda
bvishal8510 <baghel.vishal124124@gmail.com>
2018-09-17 22:48:36 +0530

    checking prime with fermet method

---
+++ prime number using fermet method.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+int power(int a, unsigned int n, int p) 
+{ 
+    int res = 1;
+    a = a % p;  
+  
+    while (n > 0) 
+    { 
+        if (n & 1) 
+            res = (res*a) % p; 
+  
+        n = n>>1; 
+        a = (a*a) % p; 
+    } 
+    return res; 
+} 
+  
+bool isPrime(unsigned int n, int k) 
+{ 
+   if (n <= 1 || n == 4)  return false; 
+   if (n <= 3) return true; 
+  
+   while (k>0) 
+   { 
+       int a = 2 + rand()%(n-4);   
+  
+       if (power(a, n-1, n) != 1) 
+          return false; 
+  
+       k--; 
+    } 
+  
+    return true; 
+} 
+  
+int main() 
+{ 
+    int k = 3;
+    int a,b; 
+    cout << "Enter numbers ";
+    cin >> a >> b;
+    isPrime(a, k)?  cout << " true\n": cout << " false\n"; 
+    isPrime(b, k)?  cout << " true\n": cout << " false\n"; 
+    return 0; 
+} 

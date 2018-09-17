1ab7b3eb200524394da2e60d771a1c0b06a05b55
bvishal8510 <baghel.vishal124124@gmail.com>
2018-09-17 22:38:25 +0530

    check whether number is prime or not

---
+++ prime number optimized.cpp
@@ -0,0 +1,36 @@
+// A optimized school method based C++ program to check 
+// if a number is prime 
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+bool isPrime(int n) 
+{ 
+    // Corner cases 
+    if (n <= 1)  return false; 
+    if (n <= 3)  return true; 
+  
+    // This is checked so that we can skip  
+    // middle five numbers in below loop 
+    if (n%2 == 0 || n%3 == 0) return false; 
+  
+    for (int i=5; i*i<=n; i=i+6)
+    {
+        cout << "i is " << i << "n%i is " << n%i;
+        if (n%i == 0 || n%(i+2) == 0) 
+           return false; 
+    }
+  
+    return true; 
+} 
+  
+  
+// Driver Program to test above function 
+int main() 
+{ 
+    int a,b;
+    cout << "Enter a number";
+    cin >> a >> b;
+    isPrime(a)?  cout << " true\n": cout << " false\n"; 
+    isPrime(b)?  cout << " true\n": cout << " false\n";
+    return 0; 
+} 
